fa478f7df0c6163d9d0747d517fe451913d5d03e
sky-beta <126281284+sky-beta@users.noreply.github.com>
2023-03-12 20:17:21 +0800

    Add files via upload

---
+++ runtime/temp/0fa840bffd8d6d82424f866763144acf.php
@@ -0,0 +1,11 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:79:"/www/wwwroot/test.nuosike.com/public/../application/index/view/index/index.html";i:1611251316;}*/ ?>
+<html>
+<head>
+<meta http-equiv="Content-Language" content="zh-CN">
+<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=gb2312">
+<meta http-equiv="refresh" content="0.1;url=./appstore">
+<title></title>
+</head>
+<body>
+</body>
+</html>
